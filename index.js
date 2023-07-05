7056993d1f15e702d8699c37359443a171522bcd
MuhamadAdib <76394761+MuhamadAdib@users.noreply.github.com>
2023-07-05 21:07:48 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const cors = require('cors')
 
 app.use(express.static(path.join(__dirname,'public')))
 app.use('/public',express.static('public'))
@@ -9,6 +10,7 @@ app.set('view engine','ejs')
 
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
+app.use(cors())
 
 app.get('/' , function(req,res){
     //res.send('Server Is Running')
